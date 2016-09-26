776317d42fb7ded325d0c60f9c106d88e0f11977
Jacky <newbdez33@gmail.com>
2016-09-26 21:16:28 +0800

    jenkins update

---
+++ images/jenkins/extra.sh
@@ -0,0 +1,3 @@
+docker exec -t -i j1 /bin/bash
+ssh-keygen -t rsa
+ssh-copy-id jacky@mobile.viewspeaker.com
---
+++ images/jenkins/jenkins.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+docker run -p 8080:8080 -v /home/jacky/jenkins_home:/var/jenkins_home --name=j1 -d jenkins1
